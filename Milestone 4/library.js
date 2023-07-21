fdf73ac3b8880990c0b10cbecbe1b760a7d62651
Noob_Hacker <gauravjais4491@gmail.com>
2023-07-21 11:14:16 +0530

    Added error handing in project

--- Milestone 4/library.js
+++ Milestone 4/library.js
@@ -1,58 +1,76 @@
 // This is the main file for the Li(brary Management System
-const prompt=require("prompt-sync")()
+const prompt = require("prompt-sync")()
 class Library {
     constructor() {
         this.books = [];
     }
-    addBook = (myBook) => {
-        this.books.push(myBook)
+    addBook(book) {
+        try {
+            if (this.books.some(b => b.title === book.title)) {
+                throw new Error(`Book with title "${book.title}" already exists.`);
+            }
+            this.books.push(book);
+        } catch (error) {
+            console.error(error.message);
+        }
     }
     getBook = (title) => {
-        return this.books.find(book=>book.title===title) || null;
+        return this.books.find(book => book.title === title) || null;
     }
     getAllBooks() {
         return this.books;
     }
     removeBook(title) {
-        const bookIndex=this.books.findIndex(book=>book.title===title);
-        if(bookIndex!==-1)
-        {
-            this.books.splice(bookIndex,1)
+        try {
+            const bookIndex = this.books.findIndex((book) => book.title === title);
+            if (bookIndex === -1) {
+                throw new Error(`No book with title "${title}" exists.`);
+            }
+            this.books.splice(bookIndex, 1);
+        } catch (error) {
+            console.error(error.message);
         }
     }
     findBook = (title) => {
-        return this.books.find(myBook=>myBook.title===title) || null;
+        return this.books.find(myBook => myBook.title === title) || null;
     }
-    lendBook(title,user)
-    {
-        const book=this.findBook(title)
-        if(book && !book.isBorrowed && book.numberOfCopies>0)
-        {
-            book.isBorrowed=true;
-            book.numberOfCopies-=1
-            console.log(`${user} has borrowed "${title}"`)
-        }
-        else{
-            console.log(`Sorry, "${title} is not available."`)
+    lendBook(title, user) {
+        try {
+            const book = this.findBook(title);
+            if (!book) {
+                throw new Error(`No book with title "${title}" exists.`);
+            }
+            if (book.isBorrowed || book.numberOfCopies === 0) {
+                throw new Error(`Book with title "${title}" is not available.`);
+            }
+            book.isBorrowed = true;
+            book.numberOfCopies -= 1;
+            console.log(`${user} has borrowed "${title}"`);
+        } catch (error) {
+            console.error(error.message);
         }
     }
-    returnBook=(title,user)=>{
-        const book=this.findBook(title);
-        if(book && book.isBorrowed)
-        {
-            book.isBorrowed=false;
-            book.numberOfCopies+=1
-            console.log(`${user} returned the book with titile: "${title}" ` )
-        }
-        else{
-            console.log(`Sorry, "${title}" is not borrowed.`)
+    returnBook(title, user) {
+        try {
+            const book = this.findBook(title);
+            if (!book) {
+                throw new Error(`No book with title "${title}" exists.`);
+            }
+            if (!book.isBorrowed) {
+                throw new Error(`Book with title "${title}" is not currently borrowed.`);
+            }
+            book.isBorrowed = false;
+            book.numberOfCopies += 1;
+            console.log(`${user} has returned "${title}"`);
+        } catch (error) {
+            console.error(error.message);
         }
     }
 }
 
 
 class Book {
-    constructor(title, author, ISBN,numberOfCopies) {
+    constructor(title, author, ISBN, numberOfCopies) {
         this.title = title;
         this.author = author;
         this.ISBN = ISBN;
@@ -98,9 +116,9 @@ class Book {
 const myBook = new Book()
 const myLibrary = new Library()
 
-let book1 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 10,5);
-let book2 = new Book("The Hobbit", "J.R.R. Tolkien", 5,5);
-let book3 = new Book("1984", "George Orwell", 8,5);
+let book1 = new Book("Harry Potter and the Philosopher's Stone", "J.K. Rowling", 10, 5);
+let book2 = new Book("The Hobbit", "J.R.R. Tolkien", 5, 5);
+let book3 = new Book("1984", "George Orwell", 8, 5);
 
 myLibrary.addBook(book1)
 myLibrary.addBook(book2)
@@ -108,7 +126,6 @@ myLibrary.addBook(book3)
 console.log(myLibrary.getAllBooks())
 // myLibrary.removeBook("1984")
 console.log(myLibrary.findBook("The Hobbit"))
-myLibrary.lendBook("1984","Gaurav")
-myLibrary.lendBook("1984","Harshit")
+myLibrary.lendBook("1984", "Gaurav")
+myLibrary.lendBook("1984", "Harshit")
 myLibrary.returnBook("1984", "Gaurav");
-
