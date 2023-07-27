71da6a596eab29dd92df3e1d1cd40294fd70b343
Noob_Hacker <gauravjais4491@gmail.com>
2023-07-27 16:36:19 +0530

    added saveNotes function

--- Milestone 5/main.js
+++ Milestone 5/main.js
@@ -4,57 +4,64 @@ import promisify from "util"
 
 let noteslist = []
 
-const underline=()=>{
+const underline = () => {
     console.log("-----------------------------------------------")
-}
+};
 
 const welcomeMessage = () => {
     console.log("Welcome to the Note-Taking Application!")
     underline()
-}
+};
 
 const createNote = (title, content) => {
     const newNote = new Note(title, content)
     noteslist.push(newNote)
-}
+};
 
-
-const deleteNote=(title)=>{
-    let index=noteslist.findIndex(note=>note.title===title)
-    if(index>=0)
-    {
-        noteslist.splice(index,1)
+const deleteNote = (title) => {
+    let index = noteslist.findIndex(note => note.title === title)
+    if (index >= 0) {
+        noteslist.splice(index, 1)
         console.log(`${title} is deleted from notesList`)
-        
+
     }
-    else{
+    else {
         console.log(`No such title ${title}`)
     }
     underline()
-}
+};
 
-
-const editNote=(title,newContent)=>{
-    let noteToEdit=noteslist.find(note=>note.title===title)
-    if(noteToEdit)
-    {
-        noteToEdit.content=newContent
+const editNote = (title, newContent) => {
+    let noteToEdit = noteslist.find(note => note.title === title)
+    if (noteToEdit) {
+        noteToEdit.content = newContent
         console.log(`Note with title '${title}' has been updated.`);
     }
-    else{
+    else {
         console.log(`No such title ${title}`)
     }
     underline()
 
-}
+};
+
+const saveNotes = async () => {
+    const writeFileAsync = promisify(fs.writeFile);
+    try {
+        const data = JSON.stringify(noteslist);
+        await writeFileAsync('notes.json', data);
+        console.log('Notes saved to notes.json.');
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+    }
+};
 
 const displayAllNotes = () => {
-    for (const note of noteslist) {
-        note.displayNotes();
+    for (const notes of noteslist) {
+        notes.displayNotes();
         underline()
     }
-    
-}
+};
+
 createNote('First Note', 'This is the first note.');
 createNote('Second Note', 'This is the second note.');
 createNote('Third Note', 'This is the third note.');
@@ -66,5 +73,5 @@ createNote('Third Note', 'This is the third note.');
 underline()
 welcomeMessage()
 displayAllNotes()
-editNote('First Note',"Hello Gaurav")
+editNote('First Note', "Hello Gaurav")
 displayAllNotes()
