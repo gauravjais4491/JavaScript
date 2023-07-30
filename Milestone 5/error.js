c0d150afedd80b31f333be7a077c355da59e196f
Noob_Hacker <gauravjais4491@gmail.com>
2023-07-30 17:15:47 +0530

    added error handling

---
+++ Milestone 5/error.js
@@ -0,0 +1,9 @@
+class InputValidationError extends Error{
+    constructor(message)
+    {
+        super(message)
+        this.name="InputValidationError"
+    }
+}
+
+export default{InputValidationError}
