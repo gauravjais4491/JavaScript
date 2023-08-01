aea92a164fff54149bfca915e19de3377166e544
Noob_Hacker <gauravjais4491@gmail.com>
2023-08-01 09:07:57 +0530

    Craeted Start Function to Start the Game

--- JavaScript Tutorial/Practice/Snake_Gun_Water_Game.js
+++ JavaScript Tutorial/Practice/Snake_Gun_Water_Game.js
@@ -1,22 +1,21 @@
 import PromptSync from "prompt-sync";
 const prompt = new PromptSync();
 
-let arr=["Sanke","Water","Gun"]
+let arr = ["Snake", "Water", "Gun"]
 
-let computer=Math.floor(Math.random()*arr)
+let computer = Math.floor(Math.random() * arr)
 
-console.log(" 1) Sanke \n 2) Water \n 3) Gun")
-let User=prompt(`Enter Your Choice: `)
-let isPlay=true;
 
-const inputValidation=(User)=>{
-    if(arr.includes(User))
-    {
-        
+let isPlay = true;
+
+while (isPlay) {
+    const Start=()=>{
+        underline()
+        console.log("Welcome to the Gaming World! ")
+        underline()
+        console.log(" 1) Snake \n 2) Water \n 3) Gun")
+        let User = prompt(`Enter Your Choice: `)
+        inputValidation(User)
     }
-    else{
-        console.log("Invalid Input!")
-    }
+    Start()
 }
-
-inputValidation(User)
