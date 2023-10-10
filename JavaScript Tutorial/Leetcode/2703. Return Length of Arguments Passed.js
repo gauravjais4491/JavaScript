5d9a8b99f154bb4989a9b9c117a6d66a858a2698
Noob_Hacker <gauravjais4491@gmail.com>
2023-10-10 15:52:17 +0530

    Started Leetcode

---
+++ JavaScript Tutorial/Leetcode/2703. Return Length of Arguments Passed.js
@@ -0,0 +1,7 @@
+// Write a function argumentsLength that returns the count of arguments passed to it.
+
+const count = (arr) => {
+    return arr.length;
+}
+
+console.log(count([1,2,3,4,5,null,true]));
