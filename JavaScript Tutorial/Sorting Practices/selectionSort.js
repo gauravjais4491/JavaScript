f5c0209aeeb6a141999537a5182bc35493f800b5
Noob_Hacker <gauravjais4491@gmail.com>
2024-01-04 12:20:20 +0530

    selectionSort.js

---
+++ JavaScript Tutorial/Sorting Practices/selectionSort.js
@@ -0,0 +1,19 @@
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length - 1; i++) {
+        let min = i;
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        if (min != i) {
+            arr[i] = arr[min] + arr[i]
+            arr[min] = arr[i] - arr[min]
+            arr[i] = arr[i] - arr[min]
+        }
+    }
+    return arr
+}
+
+
+console.log(selectionSort([1, 2, 42, 53, 54, 2, 6, 2, 6, 89, 4, 78]));
