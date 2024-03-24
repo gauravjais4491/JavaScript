a5178885a206818b8dd41f35cffb2fee230313c3
Noob_Hacker <gauravjais4491@gmail.com>
2024-03-24 12:29:39 +0530

    string is a palindrome or not

---
+++ JavaScript Tutorial/Practice/isPalindromeString.js
@@ -0,0 +1,21 @@
+function isPalindromeString(str) {
+    // const cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, '');
+    
+    let start = 0;
+    let end = cleanStr.length - 1;
+
+    while (start < end) {
+        if (cleanStr[start] !== cleanStr[end]) {
+            return false;
+        }
+        start++;
+        end--;
+    }
+
+    return true;
+}
+
+
+console.log(isPalindromeString("A man, a plan, a canal, Panama"));
+console.log(isPalindromeString("racecar"));
+console.log(isPalindromeString("hello")); 
